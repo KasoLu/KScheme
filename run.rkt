4a2cb83c595b7a1a997286db94b09f85ad0e134b
KasoLu <kasoalpha@gmail.com>
2019-06-23 01:07:00 +0800

    refactor run

--- run.rkt
+++ run.rkt
@@ -2,7 +2,10 @@
 
 (require "helper.rkt")
 (require "verify-scheme.rkt")
+(require "finalize-locations.rkt")
 (require "expose-frame-var.rkt")
+(require "expose-basic-blocks.rkt")
+(require "inline-basic-blocks.rkt")
 (require "flatten-program.rkt")
 (require "generate-x86-64.rkt")
 (require "debug-program.rkt")
@@ -16,10 +19,16 @@
 (define build-passes
   (list
     verify-scheme
+    finalize-locations
     expose-frame-var
+    expose-basic-blocks
+    inline-basic-blocks
     flatten-program
-    generate-x86-64))
+    generate-x86-64
+    ))
 
 (driver debug-passes build-passes invalid-tests)
 (driver debug-passes build-passes tests)
+
+
 
