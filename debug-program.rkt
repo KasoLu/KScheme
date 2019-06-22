3bdfcb2f5d7fce51b74cc7a0e6275a0c37ee4c25
KasoLu <kasoalpha@gmail.com>
2019-06-23 01:05:59 +0800

    refactor debug-program

--- debug-program.rkt
+++ debug-program.rkt
@@ -5,60 +5,90 @@
 
 (define debug-program
   (lambda (program)
-    (match program
-      [`(letrec ([,label* (lambda () ,tail*)] ...) ,tail)
-        (let 
-          ([debug-lambda (lambda (l t) `(,l (lambda () ,t)))]
-           [debug-tail* (map debug-tail tail*)])
-         `(letrec 
-            ,(map debug-lambda label* debug-tail*)
-            ,(debug-tail tail)))])))
+    ($program program)))
 
-(define debug-tail
+(define $program
+  (lambda (program)
+    (match program
+      [`(letrec ([,l* (lambda () ,b*)] ...) ,b)
+        (let ([dbg-lmd (lambda (l b) `(,l (lambda () ,b)))])
+         `(letrec 
+            ,(map dbg-lmd l* (map $body b*))
+            ,($body b)))])))
+
+(define $body
+  (lambda (body)
+    (match body
+      [`(locate ([,u* ,l*] ...) ,t)
+        (let ([dbg-bind* (lambda (u l) `(,u ,l))])
+          `(let ,(map dbg-bind* u* (map $var l*)) ,($tail t)))])))
+
+(define $tail
   (lambda (tail)
     (match tail
-      [`(begin ,effect* ... ,tail)
-       `(begin ,@(map debug-effect effect*) ,(debug-tail tail))]
+      [`(if ,p ,t-t ,t-f)
+       `(if ,($pred p) ,($tail t-t) ,($tail t-f))]
+      [`(begin ,e* ... ,t)
+       `(begin ,@(map $effect e*) ,($tail t))]
       [`(,triv)
-       `(,(debug-triv triv))])))
+       `(,($triv triv))])))
 
-(define debug-effect
+(define $pred
+  (lambda (pred)
+    (match pred
+      [`(true)
+       `(true)]
+      [`(false)
+       `(false)]
+      [`(if ,p-1 ,p-2 ,p-3)
+       `(if ,($pred p-1) ,($pred p-2) ,($pred p-3))]
+      [`(begin ,e* ... ,p)
+       `(begin ,@(map $effect e*) ,($pred p))]
+      [`(,r ,t-1 ,t-2)
+       `(,r ,($triv t-1) ,($triv t-2))])))
+
+(define $effect
   (lambda (effect)
     (match effect
+      [`(nop)
+       `(nop)]
       [`(set! ,var (,binop ,triv-1 ,triv-2))
-       `(set-box! ,(debug-var var) 
-                   (int64-truncate (,binop ,(debug-triv triv-1) ,(debug-triv triv-2))))]
+       `(set-box! ,($var var) 
+                   (int64-truncate (,binop ,($triv triv-1) ,($triv triv-2))))]
       [`(set! ,var ,triv)
-       `(set-box! ,(debug-var var) ,(debug-triv triv))])))
+       `(set-box! ,($var var) ,($triv triv))]
+      [`(if ,p ,e-1 ,e-2)
+       `(if ,($pred p) ,($effect e-1) ,($effect e-2))]
+      [`(begin ,e* ... ,e)
+       `(begin ,@(map $effect e*) ,($effect e))])))
 
-(define debug-triv
+(define $triv
   (lambda (triv)
     (match triv
       [(? var?)
-      `(unbox ,(debug-var triv))]
+      `(unbox ,($var triv))]
       [(? int?)
        (begin triv)]
       [(? label?)
        (begin triv)])))
 
-(define debug-var
+(define $var
   (lambda (var)
     (match var
+      [(? uvar?)
+       (begin var)]
       [(? reg?)
        (begin var)]
-      [(? frame-var?)
-      `(vector-ref stack (+ (/ (unbox rbp) 8) ,(frame-var->index var)))])))
+      [(? fvar?)
+      `(vector-ref stack (+ (/ (unbox rbp) 8) ,(fvar->index var)))])))
 
 ; ----- test ----- ;
-;(test debug-program
-;      '(letrec
-;         ([f$1 (lambda ()
-;                 (begin
-;                   (set! fv0 rax)
-;                   (set! rax (+ rax rax))
-;                   (set! rax (+ rax fv0))
-;                   (r15)))])
-;         (begin
-;           (set! rax 17)
-;           (f$1))))
+;(test $program 
+;      '(letrec () 
+;         (locate ()
+;                 (begin 
+;                   (set! rax 5)
+;                   (r15)))))
+;(test $program
+;      '(letrec ((double$0 (lambda () (locate () (begin (set! rax (+ rax rax)) (r15)))))) (locate () (begin (set! rax 10) (double$0)))))
 
